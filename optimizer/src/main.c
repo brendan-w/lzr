51b93660eed78f0c7849b1fad10118d88855ae2f
Brendan Whitfield <brendanw@windworksdesign.com>
2015-07-04 02:43:05 -0400

    added path splitting based on angle deviation

--- optimizer/src/main.c
+++ optimizer/src/main.c
@@ -13,17 +13,20 @@ lzr_path_buffer  paths;
 #define paths(i)  (paths.paths[i])
 
 
-//create some bogus data to play with
 static void load()
 {
-    for(int i = 0; i < POINT_BUFFER_SIZE; i++)
-    {
-        lzr_point* p = &points(i);
-        p->x = i;
-        p->y = i;
-        p->r = p->g = p->b = 1; //non-zero color
-        p->i = (i % 4 == 0) ? 0 : 1;
-    }
+    //create some bogus data to play with
+    lzr_point p0 = { .x=0, .y=0, .r=1, .g=1, .b=1, .i=1 }; points(0) = p0;
+    lzr_point p1 = { .x=9, .y=1, .r=1, .g=1, .b=1, .i=1 }; points(1) = p1;
+    lzr_point p2 = { .x=2, .y=2, .r=1, .g=1, .b=1, .i=1 }; points(2) = p2;
+    lzr_point p3 = { .x=3, .y=3, .r=1, .g=1, .b=1, .i=1 }; points(3) = p3;
+    lzr_point p4 = { .x=4, .y=4, .r=1, .g=1, .b=1, .i=1 }; points(4) = p4;
+    lzr_point p5 = { .x=5, .y=5, .r=1, .g=1, .b=1, .i=0 }; points(5) = p5;
+    lzr_point p6 = { .x=6, .y=6, .r=1, .g=1, .b=1, .i=1 }; points(6) = p6;
+    lzr_point p7 = { .x=7, .y=7, .r=1, .g=1, .b=1, .i=1 }; points(7) = p7;
+    lzr_point p8 = { .x=8, .y=8, .r=1, .g=1, .b=1, .i=0 }; points(8) = p8;
+    lzr_point p9 = { .x=9, .y=9, .r=1, .g=1, .b=1, .i=1 }; points(9) = p9;
+
     points.length = POINT_BUFFER_SIZE;
 }
 
--- optimizer/src/optimize.h
+++ optimizer/src/optimize.h
@@ -9,30 +9,30 @@
 #include <lzr_types.h>
 
 
-//constants
-# define PI 3.14159265358979323846
-
 //tunable settings
-#define POINT_BUFFER_SIZE 10        //
-#define PATH_SPLIT_ANGLE (180 - 30) //max number of degrees before the point is considered a corner
+#define POINT_BUFFER_SIZE 10      //
+#define PATH_SPLIT_ANGLE (PI / 8) //max angle deviation for points to be considered contiguous
+
 
 //computed settings
 #define PATH_BUFFER_SIZE (POINT_BUFFER_SIZE / 2) //the shortest path is 2 points
 
+//constants
+# define PI 3.14159265358979323846
 
 //functions
-#define DISTANCE(a, b)    ( (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) ) //square of the distance between two points
-#define ANGLE(a, b)       ( atan2(b.y - a.y, b.x - a.y) * -1 ) //the angle from point A to point B on range (-PI, PI] in screen coords
-#define ANGLE_NORM(a)     ( fmod(a, PI) ) //constrains angles to (-PI, PI]
-#define ANGLE_OPPOSITE(a) ( ANGLE_NORM(a + PI) )
-#define IS_BLANKED(p)     ( (p.i == 0) || (p.r + p.g + p.b == 0) ) // returns boolean for whether the given point is blanked
+#define DISTANCE(a, b)        ( (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) ) //square of the distance between two points
+#define ANGLE(a, b)           ( atan2(b.y - a.y, b.x - a.y) * -1 ) //the angle from point A to point B on range (-PI, PI] in screen coords
+#define ANGLE_NORM(a)         ( fmod(a, PI) ) //constrains angles to (-PI, PI]
+#define ANGLE_OPPOSITE(a)     ( ANGLE_NORM(a + PI) )
+#define ANGLE_FORMED(a, b, c) ( ANGLE(a, b) - ANGLE(b, c) ) //angle formed by three points [0, PI] (0 = straight line, PI = folded backward)
+#define IS_BLANKED(p)         ( (p.i == 0) || (p.r + p.g + p.b == 0) ) // returns boolean for whether the given point is blanked
 
 
 //struct defining a "continguous" segment of the lasers path
 typedef struct {
-    //order of start vs. end points is irrelevant here
-    size_t ai;    //index of a starting point (or, the end point)
-    size_t bi;    //index of a ending point (or, the start point)
+    size_t ai;      //index of front point
+    size_t bi;      //index of back point (guaranteed that bi > ai)
     double a_angle; //the angle for entering the path at point A (radians)
     double b_angle; //the angle for entering the path at point B (radians)
     bool cycle;     //whether or not this path is cyclic (points A and B are equal)
