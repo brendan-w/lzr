aced3096cd0a255a35e1cb836e2ce1fc968a5097
Brendan Whitfield <brendanw@windworksdesign.com>
2015-07-27 21:36:33 -0400

    only malloc the etherdream points once

--- lzrd/lzrd.c
+++ lzrd/lzrd.c
@@ -19,10 +19,11 @@ typedef struct etherdream etherdream;
 typedef struct etherdream_point etherdream_point;
 
 
-static void*       zmq_ctx;
-static void*       rx;
-static lzr_frame*  frame;
-static etherdream* dac;
+static void*             zmq_ctx;
+static void*             rx;
+static lzr_frame*        frame;
+static etherdream*       dac;
+static etherdream_point* ether_points;
 
 
 static void send_frame()
@@ -32,21 +33,17 @@ static void send_frame()
     {
         printf("RECV frame (%d points)\n", frame->n_points);
 
-        etherdream_point* points = (etherdream_point*) calloc(sizeof(etherdream_point), frame->n_points);
-
         for(size_t i = 0; i < frame->n_points; i++)
         {
-            points[i].x = frame->points[i].x;
-            points[i].y = frame->points[i].y;
-            points[i].r = frame->points[i].r;
-            points[i].g = frame->points[i].g;
-            points[i].b = frame->points[i].b;
-            points[i].i = frame->points[i].i;
+            ether_points[i].x = frame->points[i].x;
+            ether_points[i].y = frame->points[i].y;
+            ether_points[i].r = frame->points[i].r;
+            ether_points[i].g = frame->points[i].g;
+            ether_points[i].b = frame->points[i].b;
+            ether_points[i].i = frame->points[i].i;
         }
 
-        int rc = etherdream_write(dac, points, frame->n_points, 30000, -1);
-        printf("SEND frame (%d points)\n", rc);
-        free(points);
+        etherdream_write(dac, ether_points, frame->n_points, 30000, -1);
     }
     //else, dump the frame, an old one is still being drawn
     //TODO: ^ is this really a good idea? Could create a stutterring animation
@@ -76,10 +73,11 @@ static int loop()
 //main laser client
 int main()
 {
-    int rc  = 0;
-    zmq_ctx = zmq_ctx_new();
-    rx      = lzr_create_rx(zmq_ctx);
-    frame   = (lzr_frame*) malloc(sizeof(lzr_frame));
+    int rc       = 0;
+    zmq_ctx      = zmq_ctx_new();
+    rx           = lzr_create_rx(zmq_ctx);
+    frame        = (lzr_frame*) malloc(sizeof(lzr_frame));
+    ether_points = (etherdream_point*) calloc(sizeof(etherdream_point), FRAME_MAX_POINTS);
 
     etherdream_lib_start();
 
@@ -108,6 +106,8 @@ int main()
 
     etherdream_stop(dac);
     etherdream_disconnect(dac);
+
+    free(ether_points);
     free(frame);
     zmq_close(rx);
     zmq_ctx_destroy(zmq_ctx);
