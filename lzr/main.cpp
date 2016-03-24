d04c56a841bcea183f5a193973c5c1fd1f9a3fe8
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-23 22:21:35 -0400

    added basic stylesheet with a darker colorscheme

--- lzr/main.cpp
+++ lzr/main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //load LZR's stylesheet
+    QFile style_file("style.qss");
+    style_file.open(QFile::ReadOnly);
+    QString style = QLatin1String(style_file.readAll());
+    a.setStyleSheet(style);
+
     LZREditor w;
     w.show();
 
