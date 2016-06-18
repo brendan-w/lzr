5c107694a875e0032da6185fe7c41073ba1b6529
Brendan Whitfield <brendanw@windworksdesign.com>
2016-06-17 23:34:54 -0400

    wrote util for finding the perpendicular intersection of line and point

--- lzr/src/utils.h
+++ lzr/src/utils.h
@@ -8,9 +8,22 @@
 
 const QRectF frame_bounds(-1.0, -1.0, 2.0, 2.0);
 
-inline float distance_between_points(QPointF& a, QPointF& b)
+inline float distance_between_points(const QPointF& a, const QPointF& b)
 {
     return QVector2D(a).distanceToPoint(QVector2D(b));
+}
+
+inline QPointF perpendicular_intersection(const QPointF& a, const QPointF& b, const QPointF& x)
+{
+    QVector2D ab(b - a);
+    QVector2D ax(x - a);
+
+    QVector2D intersection(ab);
+    intersection.normalize();
+    intersection *= QVector2D::dotProduct(intersection, ax);
+    intersection += QVector2D(a);
+
+    return intersection.toPointF();
 }
 
 QPointF constrain_and_snap(QPointF p, bool snap=false, int divisions=1);
