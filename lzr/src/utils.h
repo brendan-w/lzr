371ecbad16cd257ea2da6d81023e4711c7ef6870
Brendan Whitfield <brendanw@windworksdesign.com>
2016-06-18 21:47:40 -0400

    correct math for determining if point is inside line

--- lzr/src/utils.h
+++ lzr/src/utils.h
@@ -13,23 +13,26 @@ inline float distance_between_points(const QPointF& a, const QPointF& b)
     return QVector2D(a).distanceToPoint(QVector2D(b));
 }
 
-inline QPointF perpendicular_intersection(const QPointF& a,
-                                          const QPointF& b,
-                                          const QPointF& x,
-                                          bool& inside)
+inline bool perpendicular_intersection(const QPointF& a,
+                                       const QPointF& b,
+                                       const QPointF& x,
+                                       QPointF& intersection)
 {
     QVector2D ab(b - a);
     QVector2D ax(x - a);
 
-    QVector2D intersection(ab);
-    intersection.normalize();
-    intersection *= QVector2D::dotProduct(intersection, ax);
-    intersection += QVector2D(a);
+    QVector2D i(ab);
+    i.normalize();
+    i *= QVector2D::dotProduct(i, ax);
+    i += QVector2D(a);
 
-    float dot = QVector2D::dotProduct(ab, ax);
-    inside = (dot > 0.0) && (dot < 2.0);
+    intersection = i.toPointF();
 
-    return intersection.toPointF();
+    QVector2D ai(intersection - a);
+    float dot = QVector2D::dotProduct(ab, ai);
+    bool inside = (dot > 0.0) && (ai.lengthSquared() < ab.lengthSquared());
+
+    return inside;
 }
 
 QPointF constrain_and_snap(QPointF p, bool snap=false, int divisions=1);
