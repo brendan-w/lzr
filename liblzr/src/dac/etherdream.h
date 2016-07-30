9d93e64b8309d82905c319ea79862a9c4126058a
Brendan Whitfield <brendanw@windworksdesign.com>
2016-07-29 20:30:23 -0400

    removed stray references to old pps setting

--- liblzr/src/dac/etherdream.h
+++ liblzr/src/dac/etherdream.h
@@ -17,13 +17,10 @@ public:
 
     int send(Frame frame);
     int stop();
-    void pps(int pps);
-    int pps();
 
 private:
     struct etherdream* dac;
     std::vector<struct etherdream_point> buffer;
-    int _pps;
 };
 
 } // namespace lzr
