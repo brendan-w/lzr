91b81ee9e537161ce9e4445cadb1f353d5344332
Brendan Whitfield <brendanw@windworksdesign.com>
2017-02-11 14:28:08 -0800

    reduce duplicates in place

--- liblzr/src/reduce.cpp
+++ liblzr/src/reduce.cpp
@@ -39,27 +39,27 @@ int reduce_blanks(Frame& frame)
 
 int remove_duplicates(Frame& frame)
 {
-    //setup a buffer to build the finished product
-    //TODO: rewrite this to operate in place
-    Frame output;
-
     Point prev(0.0, 0.0, 0, 0, 0, 0); // initialize as blanked
 
-    for(const Point& point : frame)
+    auto it = frame.begin();
+    while(it != frame.end())
     {
+        const Point& point = *it;
         const bool discard = prev.is_lit() &&
                              point.is_lit() &&
                              point.same_position_as(prev);
-
-        if(!discard)
-        {
-            output.add(point);
-        }
-
         prev = point;
+
+        if(discard)
+        {
+            it = frame.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
     }
 
-    frame = output;
     return LZR_SUCCESS;
 }
 
