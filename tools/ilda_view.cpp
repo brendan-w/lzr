d295e56f33504fbb5797ecc6ba71771f6dd4409b
Brendan Whitfield <brendanw@windworksdesign.com>
2020-12-23 22:20:36 -0800

    Added scan progression tracing to ilda-view

--- tools/ilda_view.cpp
+++ tools/ilda_view.cpp
@@ -11,8 +11,9 @@ using namespace lzr;
 
 #define DEFAULT_WINDOW_SIZE 1024
 #define POINT_SIZE 3
-#define FPS 15
 
+static constexpr size_t play_period_ms = 70;
+static constexpr size_t animate_scan_period_ms = 10;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -25,16 +26,39 @@ bool play = false;
 bool show_blanks = false;
 bool show_points = false;
 
+// point scanning display
+bool show_scan = false;
+int show_scan_n_points = 0;  // -1 indicates "show all"
+bool animate_scan = false;
+
 static inline void next_frame()
 {
     frame_n++;
-    frame_n = (frame_n >= (int)frames.size()) ? 0 : frame_n;
+    frame_n = (frame_n >= static_cast<int>(frames.size())) ? 0 : frame_n;
 }
 
 static inline void prev_frame()
 {
     frame_n--;
     frame_n = (frame_n < 0) ? frames.size() - 1 : frame_n;
+}
+
+static inline void show_next_point()
+{
+    show_scan_n_points++;
+    if (show_scan_n_points >= static_cast<int>(frames[frame_n].size()))
+    {
+        show_scan_n_points = 0;
+    }
+}
+
+static inline void hide_prev_point()
+{
+    show_scan_n_points--;
+    if (show_scan_n_points < 0)
+    {
+        show_scan_n_points = frames[frame_n].size() - 1;
+    }
 }
 
 static inline int lzr_coord_to_screen(double v, bool invert)
@@ -82,6 +106,7 @@ static inline SDL_Color lzr_color_to_screen(Point p)
     return c;
 }
 
+
 static void render_line(const Point& p1, const Point& p2)
 {
     SDL_Point sp1 = lzr_point_to_sdl_point(p1);
@@ -104,6 +129,10 @@ static void render_line(const Point& p1, const Point& p2)
     }
 }
 
+static bool should_render(int i) {
+    return play || (show_scan ? i < show_scan_n_points : true);
+}
+
 static void render()
 {
     // clear the screen to black
@@ -118,15 +147,9 @@ static void render()
     Frame& frame = frames[frame_n];
 
     //NOTE: using int's to avoid rollover problems with -1 (empty frames)
-    for(int i = 0; i < ((int)frame.size() - 1); i++)
+    for(int i = 0; (i < ((int)frame.size() - 1)) && should_render(i+1); i++)
     {
         render_line(frame[i], frame[i+1]);
-    }
-
-    // Draw the return-to-home line, if there is any
-    if (!frame.front().same_position_as(frame.back()))
-    {
-        render_line(frame.back(), frame.front());
     }
 
     //draw points overtop of the lines
@@ -134,8 +157,9 @@ static void render()
     {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        for(Point p : frame)
+        for(size_t i = 0; (i < frame.size()) && should_render(i); i++)
         {
+            const Point& p = frame[i];
             SDL_Rect r = lzr_point_to_sdl_rect(p);
             SDL_RenderFillRect(renderer, &r);
         }
@@ -147,11 +171,9 @@ static void render()
 static void loop()
 {
     SDL_Event e;
-    bool running = true;
-
-    while(running)
+    while(true)
     {
-        if(!play)
+        if(!(play || animate_scan))
         {
             // if we're not playing, stop, and wait for an event
             SDL_WaitEvent(NULL);
@@ -164,13 +186,16 @@ static void loop()
             {
                 //dispatch SDL events to their respective handlers
                 case SDL_QUIT:
-                    running = false;
-                    break;
+                    return;
                 case SDL_KEYDOWN:
                     switch(e.key.keysym.sym)
                     {
+                        case SDLK_ESCAPE:
+                            return;
                         case SDLK_SPACE:
                             play = !play;
+                            show_scan = false;
+                            animate_scan = false;
                             break;
                         case SDLK_b:
                             show_blanks = !show_blanks;
@@ -178,22 +203,53 @@ static void loop()
                         case SDLK_p:
                             show_points = !show_points;
                             break;
+                        case SDLK_s:
+                            play = false;
+                            show_scan = !show_scan;
+                            animate_scan = !animate_scan;
+                            break;
                         case SDLK_LEFT:
-                            prev_frame();
+                            // Disable frame and point scroll when we're doing any sort of animation
+                            if (animate_scan || play) break;
+                            if (!show_scan) prev_frame();
+                            else if (show_scan) hide_prev_point();
                             break;
                         case SDLK_RIGHT:
-                            next_frame();
+                            // Disable frame and point scroll when we're doing any sort of animation
+                            if (animate_scan || play) break;
+                            if (!show_scan) next_frame();
+                            else if (show_scan) show_next_point();
+                            break;
+                        case SDLK_RSHIFT:
+                        case SDLK_LSHIFT:
+                            play = false;
+                            show_scan = true;
+                            animate_scan = false;
+                            break;
+                    }
+                    break;
+                case SDL_KEYUP:
+                    switch(e.key.keysym.sym)
+                    {
+                        case SDLK_RSHIFT:
+                        case SDLK_LSHIFT:
+                            show_scan = false;
+                            animate_scan = false;
                             break;
                     }
                     break;
                 case SDL_MOUSEWHEEL:
-                    if(e.wheel.y > 0)
+                    // Disable frame and point scroll when we're doing any sort of animation
+                    if (animate_scan || play) break;
+                    if(!show_scan)
                     {
-                        next_frame();
+                        if(e.wheel.y > 0) next_frame();
+                        else if(e.wheel.y < 0) prev_frame();
                     }
-                    else if(e.wheel.y < 0)
+                    else if(show_scan)
                     {
-                        prev_frame();
+                        if(e.wheel.y > 0) show_next_point();
+                        else if(e.wheel.y < 0) hide_prev_point();
                     }
                     break;
                 case SDL_WINDOWEVENT:
@@ -211,18 +267,37 @@ static void loop()
         if(play)
         {
             next_frame();
+            SDL_Delay(play_period_ms);
+        }
+        else if (animate_scan)
+        {
+            show_next_point();
+            // SDL_Delay(animate_scan_period_ms);
         }
 
-        SDL_Delay(1000 / FPS);
     }
 }
 
+void keybindings()
+{
+    printf("Nav Keys:\n");
+    printf("    [esc]                      Quit\n");
+    printf("    [space]                    Play/pause animation\n");
+    printf("    [left]/[right]/[scroll]    Previous/next frame. Mouse scroll Only when animation is paused\n");
+    printf("    [shift] (HOLD)             Press and hold to enter scan pattern observation mode. Will pause animation.\n");
+    printf("                               Use [left]/[right]/[scroll] to hide/reveal more points.\n");
+    printf("Display Keys:\n");
+    printf("    s    Toggle animated scan pattern. Will pause animation.\n");
+    printf("    b    Toggle visibility of blanked lines\n");
+    printf("    p    Toggle point markers\n");
+}
 
 int main(int argc, char* argv[])
 {
     if(argc <= 1)
     {
-        fprintf(stderr, "Usage: ilda-view <ilda file>\n");
+        fprintf(stderr, "Usage: ilda-view <ilda file>\n\n");
+        keybindings();
         return EXIT_SUCCESS;
     }
 
@@ -237,6 +312,8 @@ int main(int argc, char* argv[])
     printf("Found %zu projector(s)\n", ilda_projector_count(f));
     printf("Found %zu frames for projector 0\n", ilda_frame_count(f, 0));
 
+    keybindings();
+
     if(ilda_read(f, 0, frames) != LZR_SUCCESS)
     {
         fprintf(stderr, "Error loading frames\n");
@@ -245,6 +322,17 @@ int main(int argc, char* argv[])
 
     ilda_close(f);
 
+    // Simulate any return-to-home lines within single frames by adding
+    // jumps if the laser isn't already at the beginning.
+    for (Frame& frame : frames)
+    {
+        if (!frame.front().same_position_as(frame.back()))
+        {
+            frame.add(frame.front());
+        }
+    }
+
+    // Only consider this an animation if there are multiple frames
     play = frames.size() > 1;
 
     //start SDL
